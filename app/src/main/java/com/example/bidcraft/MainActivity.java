5358db6ea0037fd15db61d829582866b6b3db036
farhan7871 <ezyachyar2@gmail.com>
2020-12-09 14:21:56 +0700

    Update deskripsi kata

--- app/src/main/java/com/example/bidcraft/MainActivity.java
+++ app/src/main/java/com/example/bidcraft/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.bidcraft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivclay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ivclay = findViewById(R.id.ivclay);
+
+        ivclay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Intent gotanahliat = new Intent(MainActivity.this,CategoryClayActivity.class);
+            startActivity(gotanahliat);
+            }
+        });
     }
+
 }
