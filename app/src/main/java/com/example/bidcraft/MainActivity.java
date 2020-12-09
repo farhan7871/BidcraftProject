b416867c43957a58ab66eda2f17c6abed42dd259
farhan7871 <ezyachyar2@gmail.com>
2020-12-09 16:07:38 +0700

    Fix intent main page

--- app/src/main/java/com/example/bidcraft/MainActivity.java
+++ app/src/main/java/com/example/bidcraft/MainActivity.java
@@ -9,21 +9,65 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView ivclay;
+    ImageView ivclay, ivclay2, ivfiber, ivmetal, ivstone, ivwood, ivplastic ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ivclay2 = findViewById(R.id.ivclay2);
+        ivfiber = findViewById(R.id.ivfiber);
+        ivmetal = findViewById(R.id.ivmetal);
+        ivstone = findViewById(R.id.ivstone);
+        ivwood = findViewById(R.id.ivwood);
+        ivplastic = findViewById(R.id.ivplastic);
 
-        ivclay = findViewById(R.id.ivclay);
-
-        ivclay.setOnClickListener(new View.OnClickListener() {
+        ivclay2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            Intent gotanahliat = new Intent(MainActivity.this,CategoryClayActivity.class);
-            startActivity(gotanahliat);
+            Intent gotoclay = new Intent(MainActivity.this,CategoryClayActivity.class);
+            startActivity(gotoclay);
+            }
+        });
+
+        ivfiber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotofiber = new Intent(MainActivity.this,CategoryFiberActivity.class);
+                startActivity(gotofiber);
+            }
+        });
+
+        ivmetal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotometal = new Intent(MainActivity.this,CategoryMetalActivity.class);
+                startActivity(gotometal);
+            }
+        });
+
+        ivstone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotostone = new Intent(MainActivity.this,CategoryStoneActivity.class);
+                startActivity(gotostone);
+            }
+        });
+
+        ivwood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotowood = new Intent(MainActivity.this,CategoryWoodActivity.class);
+                startActivity(gotowood);
+            }
+        });
+
+        ivplastic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoplastic = new Intent(MainActivity.this,CategoryPlasticActivity.class);
+                startActivity(gotoplastic);
             }
         });
     }
